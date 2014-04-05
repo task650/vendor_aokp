b66cc188fdb9bda57eae0645e5d770e57eada316
task650 <task650@gmail.com>
2014-04-05 12:35:37 -0700

    Fixup jf

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/hammerhead.mk \
-    $(LOCAL_DIR)/jflteatt.mk \
+    $(LOCAL_DIR)/jflte.mk \
     $(LOCAL_DIR)/lge_mako.mk \
     $(LOCAL_DIR)/manta.mk 
     
